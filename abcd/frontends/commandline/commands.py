94aa1a9e809342183e06411005884edcb762b7b2
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2024-01-22 15:16:05 +0000

    Add option to disabled SSL

--- abcd/frontends/commandline/commands.py
+++ abcd/frontends/commandline/commands.py
@@ -9,16 +9,17 @@ logger = logging.getLogger(__name__)
 
 
 @init_config
-def login(*, config, name, url, **kwargs):
+def login(*, config, name, url, disable_ssl=False, **kwargs):
     logger.info(
         f"login args: \nconfig:{config}, name:{name}, url:{url}, kwargs:{kwargs}"
     )
     from abcd import ABCD
 
-    db = ABCD.from_url(url=url)
+    db = ABCD.from_url(url=url, use_ssl=(not disable_ssl))
     info = db.info()
 
     config["url"] = url
+    config["use_ssl"] = not disable_ssl
     config.save()
 
     print("Successfully connected to the database!")
